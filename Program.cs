b9ce512bbb05da371171b6a97180783ad1a05b94
Sophia Okosodo <sophieokosodo@gmail.com>
2022-09-29 05:55:24 +0100

    Algorithm Working

--- Program.cs
+++ Program.cs
@@ -54,7 +54,7 @@ namespace Arrays
 
 
 
-            string[,] cars =
+            /*string[,] cars =
             {
                 { "Corolla", "Camry", "Venza" },
                 { "Civic", "Pilot", "Accord" },
@@ -62,10 +62,10 @@ namespace Arrays
             };
 
 
-            /*foreach (string cr in cars)
+            foreach (string cr in cars)
             {
                 Console.WriteLine(cr);
-            }*/
+            }
 
 
             for (int i = 0; i < cars.GetLength(0); i++)
@@ -75,7 +75,147 @@ namespace Arrays
                     Console.Write(cars[i, j]+" ");
                 }
                 Console.WriteLine();
+            }*/
+
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\tSHOPPING MALL ");
+            Console.WriteLine("------------------------------");
+
+            
+            Console.WriteLine("\n \n>>> Our Shopping Mall Items\n");
+            string[,] items = {
+                {"---------", "---------------", "----------------" },
+                {"   Id.   ", "|     Item     ", "|   Price($)   |" },
+                {"---------", "---------------", "----------------" },
+                {"1        ", "|Laptop        ", "|200.00        |" },
+                {"2        ", "|Toothbrush    ", "|50.00         |" },
+                {"3        ", "|T-shirt       ", "|600.00        |" }
+            };
+
+            for (int i = 0; i < items.GetLength(0); i++)
+            {
+                for (int j = 0; j < items.GetLength(1); j++)
+                {
+                    Console.Write(items[i, j]);
+                }
+                Console.WriteLine();
             }
+
+            Console.WriteLine("\n \nHow many items do you want to buy?");
+            int itemsNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("\n \n>>> Please Input the id of the item you want to buy and press enter.");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            id = validId(id);
+
+
+            Console.Write("\n \n>>> Enter the quantity you want to buy: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            int row = itemsNumber;
+            
+            int column = 5;
+
+            Console.WriteLine("\n>>> Your Items Bought\n");
+
+            string[,] itemsBought = new string[row, column];
+
+            
+            for (int i = 0; i < 30; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    
+                    switch (id)
+                    {
+                        case 1:
+                            itemsBought[i, 0] = "|1        ";
+                            itemsBought[i, 1] = "|Laptop        ";
+                            itemsBought[i, 2] = "|200.00        ";
+                            itemsBought[i, 3] = "|" + Convert.ToString(quantity) + "        ";
+                            itemsBought[i, 4] = "|" + Convert.ToString(200 * quantity) + "       ";
+                            break;
+                        case 2:
+                            itemsBought[i, 0] = "|2         ";
+                            itemsBought[i, 1] = "|Toothbrush    ";
+                            itemsBought[i, 2] = "|50.00         ";
+                            itemsBought[i, 3] = "|" + Convert.ToString(quantity) + "        ";
+                            itemsBought[i, 4] = "|" + Convert.ToString(50 * quantity) + "        ";
+                            break;
+                        case 3:
+                            itemsBought[i, 0] = "|3          ";
+                            itemsBought[i, 1] = "|T-shirt       ";
+                            itemsBought[i, 2] = "|600.00        ";
+                            itemsBought[i, 3] = "|" +Convert.ToString(quantity) + "         ";
+                            itemsBought[i, 4] = "|" +Convert.ToString(600 * quantity) + "         ";
+                            break;
+                    }
+                   /* Console.Write(itemsBought[i, j]);*/
+                }
+                Console.WriteLine("\n \n \n>>>Do you want to buy another item? If yes write y, if no write n");
+
+                string option = Console.ReadLine();
+
+                if(option.ToLower() == "y")
+                {
+
+                    Console.WriteLine("\n \n>>> Our Shopping Mall Items\n");
+                    string[,] item = {
+                        {"---------", "---------------", "----------------" },
+                        {"   Id.   ", "|     Item     ", "|   Price($)   |" },
+                        {"---------", "---------------", "----------------" },
+                        {"1        ", "|Laptop        ", "|200.00        |" },
+                        {"2        ", "|Toothbrush    ", "|50.00         |" },
+                        {"3        ", "|T-shirt       ", "|600.00        |" }
+                    };
+
+                    for (int x = 0; x < items.GetLength(0); x++)
+                    {
+                        for (int y = 0; y < items.GetLength(1); y++)
+                        {
+                            Console.Write(item[x, y]);
+                        }
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine("\n \n>>> Please Input the id of the item you want to buy and press enter.");
+                    id = Convert.ToInt32(Console.ReadLine());
+
+                    id = validId(id);
+
+                    Console.Write("\n \n>>> Enter the quantity you want to buy: ");
+                    quantity = Convert.ToInt32(Console.ReadLine());
+
+                    continue;
+
+                }
+                else
+                {
+                    break;
+                }
+            
+            }
+
+
+            Console.WriteLine(">>>>Your Items");
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("|   Id    |    Item     |   Price($)   |  Quantity |  SubTotal |");
+            Console.WriteLine("----------------------------------------------------------------");
+            for (int i = 0; i < itemsBought.GetLength(0); i++)
+            {
+                for (int j = 0; j < itemsBought.GetLength(1); j++)
+                {
+                    Console.Write(itemsBought[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
 
 
 
@@ -83,5 +223,55 @@ namespace Arrays
 
             Console.ReadKey();
         }
+
+        static void getValues(int item_id,int itemno)
+        {
+            Console.WriteLine("\n \n>>> Our Shopping Mall Items\n");
+            string[,] items = {
+                {"---------", "---------------", "----------------" },
+                {"   Id.   ", "|     Item     ", "|   Price($)   |" },
+                {"---------", "---------------", "----------------" },
+                {"1        ", "|Laptop        ", "|200.00        |" },
+                {"2        ", "|Toothbrush    ", "|50.00         |" },
+                {"3        ", "|T-shirt       ", "|600.00        |" }
+            };
+
+            for (int i = 0; i < items.GetLength(0); i++)
+            {
+                for (int j = 0; j < items.GetLength(1); j++)
+                {
+                    Console.Write(items[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n \nHow many items do you want to buy?");
+            itemno = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("\n \n>>> Please Input the id of the item you want to buy and press enter.");
+            item_id = Convert.ToInt32(Console.ReadLine());
+
+            item_id = validId(item_id);
+        }
+
+        static int validId(int id)
+        {
+
+            if (id > 3 || id <= 0)
+            {
+                Console.WriteLine("Please enter a valid Id");
+                id = Convert.ToInt32(Console.ReadLine());
+                id = validId(id);
+
+            }
+          
+            return id;
+            
+            
+
+
+        }
+
     }
 }
